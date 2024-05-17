046e1e38c8dd98783456621f2510ec98d0e9f72f
DenisPALD <masgutovmihail@gmail.com>
2024-05-17 14:38:12 +0300

    [homework_3] Advanced task: restore signature

--- practicum_3/homework/advanced/max_flow.py
+++ practicum_3/homework/advanced/max_flow.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 import networkx as nx
 
-def dfs_check_path(G:nx.DiGraph, start_node:str, target_node:str, parent:dict[str, str|None]) -> bool:
+def dfs_check_path(G:nx.DiGraph, start_node:str, target_node:str, parent:dict[str, str]) -> bool:
     """
     return True, if there is a path from start_node to target_node
     fill dict parent with key:value = node:one of node parents 
@@ -26,8 +28,10 @@ def dfs_check_path(G:nx.DiGraph, start_node:str, target_node:str, parent:dict[st
 
 
 
-def max_flow(G:nx.DiGraph, s:str, t:str) -> float:
-    result: float = 0
+def max_flow(G:nx.DiGraph, s:Any, t:Any) -> int:
+    s = str(s)
+    t = str(t)
+    result = 0
     parent = dict()
     for node in G.nodes():
         parent[node] = None
@@ -47,16 +51,16 @@ def max_flow(G:nx.DiGraph, s:str, t:str) -> float:
         while (current_node != s):
             current_parent = parent[current_node]
             G[current_parent][current_node]["weight"] -= path_flow
-            if (G[parent[current_node]][current_node]["weight"] == 0):
+            if (G[current_parent][current_node]["weight"] == 0):
                 G.remove_edge(current_parent, current_node)
             G.add_edge(current_node, current_parent, weight=path_flow)
             current_node = current_parent
         
-    return result
+    return int(result)
 
 
 if __name__ == "__main__":
     # Load the graph
     G = nx.read_edgelist("practicum_3/homework/advanced/graph_1.edgelist", create_using=nx.DiGraph)
-    val = max_flow(G, s="0", t="5")
+    val = max_flow(G, s=0, t=5)
     print(f"Maximum flow is {val}. Should be 23")
